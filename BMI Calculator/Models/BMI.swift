f8c9989566fcd7546c03106f5a2e07c8a75352e4
talha-ather <taat4169@colorado.edu>
2022-03-21 10:54:51 -0600

    Added all project files

---
+++ BMI Calculator/Models/BMI.swift
@@ -0,0 +1,15 @@
+//
+//  BMI.swift
+//  BMI Calculator
+//
+//  Created by Talha on 1/6/22.
+//  Copyright © 2022 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+struct BMI{
+    let value: Float
+    let advice: String
+    let color: UIColor
+}
